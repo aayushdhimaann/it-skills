46242cbe8c06cc740db5c32b5df4aaf8ec3a9c2c
upagyasingh <upagyasinght48@gmail.com>
2024-10-18 21:11:42 +0530

    updates navbar

--- src/components/UI/NavBar.jsx
+++ src/components/UI/NavBar.jsx
@@ -4,6 +4,7 @@ import Login from "../Auth/Login";
 import Signup from "../Auth/Signup";
 import Link from "next/link";
 import { Menubar, MenubarMenu } from "@/components/ui/menubar";
+import { FiMenu, FiArrowLeft } from "react-icons/fi";
 import {
   Dialog,
   DialogContent,
@@ -15,126 +16,59 @@ import {
 import { useState } from "react";
 import { usePathname, useRouter } from "next/navigation";
 import { signOut, useSession } from "next-auth/react";
+import NavElement from "./NavElement";
 
 const NavBar = () => {
   const [isSignupDialogOpen, setSignupDialogOpen] = useState(false);
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
   const [isLoginDialogOpen, setLoginDialogOpen] = useState(false);
   const pathname = usePathname();
   const router = useRouter();
   const session = useSession();
   return (
-    <Menubar className="flex justify-between items-center px-0 sm:px-6 lg:px-10 rounded-none bg-gray-800 text-white border-none">
-      {/* Left section for Home */}
-      <MenubarMenu>
-        <Link
-          href={"/"}
-          className={`${pathname == "/" ? "underline" : "text-white"}`}
-        >
-          Home
-        </Link>
-      </MenubarMenu>
+    <Menubar className="fixed w-full flex justify-between items-center px-0 sm:px-6 lg:px-10 rounded-none bg-gray-800 text-white border-none ">
+      {!isSidebarOpen ? (
+        <FiMenu
+          className="text-2xl cursor-pointer 500px:block hidden "
+          onClick={() => setSidebarOpen(!isSidebarOpen)} // Toggle sidebar on click
+        />
+      ) : (
+        <FiArrowLeft
+          className="text-2xl cursor-pointer  500px:block hidden "
+          onClick={() => setSidebarOpen(!isSidebarOpen)} // Toggle sidebar on click
+        />
+      )}
 
-      {/* Right section for Login and Signup */}
-      <div className="flex items-center space-x-4">
+      <div className="500px:hidden flex-grow flex justify-around ">
         <MenubarMenu>
           <Link
-            href={"/courses"}
-            className={`${pathname == "/courses" ? "underline" : "text-white"}`}
+            href={"/"}
+            className={
+              `${pathname == "/" ? "underline" : "text-white"}` +
+              " flex items-center"
+            }
           >
-            Courses
+            Home
           </Link>
         </MenubarMenu>
-        <MenubarMenu>
-          <Link
-            href={"/contact"}
-            className={`${pathname == "/contact" ? "underline" : "text-white"}`}
-          >
-            Contact
-          </Link>
-        </MenubarMenu>
-        <MenubarMenu>
-          <Link
-            href={"/about"}
-            className={`${pathname == "/about" ? "underline" : "text-white"}`}
-          >
-            About Us
-          </Link>
-        </MenubarMenu>
-        {pathname == "/sign-in" ||
-        pathname == "/sign-up" ||
-        session.data !== null ? (
-          ""
-        ) : (
-          <>
-            <MenubarMenu>
-              <Dialog
-                open={isLoginDialogOpen}
-                onOpenChange={setLoginDialogOpen}
-              >
-                {pathname == "/sign-up" ? (
-                  <Button
-                    className="bg-transparent border-none shadow-none text-black hover:bg-gray-100 hover:shadow-lg"
-                    onClick={() => {
-                      router.replace("/sign-in");
-                    }}
-                  >
-                    Login
-                  </Button>
-                ) : (
-                  <DialogTrigger asChild>
-                    <Button className="bg-transparent border-none shadow-none text-white hover:bg-black hover:shadow-lg">
-                      Login
-                    </Button>
-                  </DialogTrigger>
-                )}
-                <DialogContent className="sm:max-w-[425px]">
-                  <DialogHeader>
-                    <DialogTitle>Login</DialogTitle>
-                  </DialogHeader>
-                  <Login onClose={() => setLoginDialogOpen(false)} />
-                </DialogContent>
-              </Dialog>
-            </MenubarMenu>
-            <MenubarMenu>
-              <Dialog
-                open={isSignupDialogOpen}
-                onOpenChange={setSignupDialogOpen}
-              >
-                <DialogTrigger asChild>
-                  <Button className="bg-transparent border-none shadow-none text-white hover:bg-black hover:shadow-lg">
-                    Signup
-                  </Button>
-                </DialogTrigger>
-                <DialogContent className="sm:max-w-[425px]">
-                  <DialogHeader>
-                    <DialogTitle>SignUp</DialogTitle>
-                    <DialogDescription>
-                      Signup to get started.
-                    </DialogDescription>
-                  </DialogHeader>
-                  {/* Pass onClose function */}
-                  <Signup onClose={() => setSignupDialogOpen(false)} />{" "}
-                </DialogContent>
-              </Dialog>
-            </MenubarMenu>
-          </>
-        )}
-        {session.data && (
-          <>
-            <MenubarMenu>
-              <Button
-                className="bg-transparent border-none shadow-none text-white hover:bg-gray-100 hover:shadow-lg hover:text-black p-2"
-                onClick={() => {
-                  signOut({ callbackUrl: "/sign-in" });
-                }}
-              >
-                Logout
-              </Button>
-            </MenubarMenu>
-            <MenubarMenu>| Hi, {session.data.user._username}</MenubarMenu>
-          </>
-        )}
+
+        {/* Right section for Login and Signup */}
+        <div className="flex items-center space-x-4">
+          <NavElement />
+        </div>
       </div>
+      {isSidebarOpen && (
+        <div className="500px:block hidden">
+          <div
+            className={`fixed top-10 left-0 h-full w-64  text-white transform transition-transform duration-300 ease-in-out shadow-lg z-10 `}
+            style={{ margin: "0px" }}
+          >
+            <div className=" flex h-full w-64 flex-col justify-top gap-10  items-center pt-7 backdrop-blur-xl">
+              <NavElement />
+            </div>
+          </div>
+        </div>
+      )}
     </Menubar>
   );
 };
