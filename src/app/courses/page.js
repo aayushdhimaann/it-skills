62e26f1b6817149778d120c29175c5668ef06aa5
upagyasingh <upagyasinght48@gmail.com>
2024-10-26 12:26:32 +0530

    add dark theme

--- src/app/courses/page.js
+++ src/app/courses/page.js
@@ -45,9 +45,9 @@ const Page = () => {
   }, []);
 
   return (
-    <div className="bg-gray-50 min-h-screen py-16 px-4">
+    <div className="min-h-screen py-16 px-4">
       <section id="courses" className="max-w-5xl mx-auto">
-        <h3 className="text-4xl font-semibold text-center text-gray-800 mb-8">
+        <h3 className="text-4xl font-semibold text-center  mb-8">
           Our Courses
         </h3>
 
@@ -62,7 +62,7 @@ const Page = () => {
                 aria-expanded={openCategory}
                 className="w-[250px] justify-between rounded-lg border border-gray-300 shadow-sm hover:bg-gray-100"
               >
-                <span className="text-purple-950">
+                <span>
                   {selectedCategory
                     ? category.find((cat) => cat.title === selectedCategory)
                         ?.title
@@ -100,7 +100,7 @@ const Page = () => {
                           className={cn(
                             "mr-2 h-4 w-4",
                             selectedCategory === cat.title
-                              ? "text-purple-950"
+                              ? "text-slate-400"
                               : "opacity-0"
                           )}
                         />
@@ -122,7 +122,7 @@ const Page = () => {
                 aria-expanded={openCourse}
                 className="w-[250px] justify-between rounded-lg border border-gray-300 shadow-sm hover:bg-gray-100"
               >
-                <span className="text-purple-950">
+                <span>
                   {selectedCourse
                     ? courses.find((course) => course.name === selectedCourse)
                         ?.name
@@ -175,7 +175,7 @@ const Page = () => {
                             className={cn(
                               "mr-2 h-4 w-4",
                               selectedCourse === course.name
-                                ? "text-purple-950"
+                                ? "text-slate-400"
                                 : "opacity-0"
                             )}
                           />
@@ -221,9 +221,9 @@ const Page = () => {
               .map((course) => (
                 <div
                   key={course._id}
-                  className="bg-white rounded-lg shadow-md border border-gray-300 overflow-hidden transition-transform transform hover:scale-105 hover:shadow-lg p-6"
+                  className=" rounded-lg shadow-md border border-gray-300 overflow-hidden transition-transform transform hover:scale-105 hover:shadow-lg p-6"
                 >
-                  <h4 className="text-2xl font-semibold text-gray-800 mb-2">
+                  <h4 className="text-2xl font-semibold  mb-2">
                     {course.name}
                   </h4>
                   <p className="text-gray-600">{course.description}</p>
