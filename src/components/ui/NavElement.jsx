8815e93549634dcefc69ef598d71c2e4123a09b5
aayushdhiman <aayush70dhiman@gmail.com>
2024-10-27 21:44:56 +0530

    fixed directory bugs

---
+++ src/components/ui/NavElement.jsx
@@ -0,0 +1,141 @@
+"use client";
+import { Button } from "@/components/ui/button"; // Adjust import according to your actual ShadCN component path
+import Login from "../Auth/Login";
+import Signup from "../Auth/Signup";
+import Link from "next/link";
+import { Menubar, MenubarMenu } from "@/components/ui/menubar";
+import { FiMenu, FiArrowLeft } from "react-icons/fi";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { signOut, useSession } from "next-auth/react";
+import React from "react";
+
+function NavElement() {
+  const [isSignupDialogOpen, setSignupDialogOpen] = useState(false);
+  // const [isSidebarOpen, setSidebarOpen] = useState(false);
+  const [isLoginDialogOpen, setLoginDialogOpen] = useState(false);
+  const pathname = usePathname();
+  const router = useRouter();
+  const session = useSession();
+  return (
+    <>
+      <MenubarMenu>
+        <Link
+          href={"/courses"}
+          className={`${pathname == "/courses" ? "underline" : "text-white"}`}
+        >
+          Courses
+        </Link>
+      </MenubarMenu>
+      <MenubarMenu>
+        <Link
+          href={"/contact"}
+          className={`${pathname == "/contact" ? "underline" : "text-white"}`}
+        >
+          Contact
+        </Link>
+      </MenubarMenu>
+      <MenubarMenu>
+        <Link
+          href={"/about"}
+          className={`${pathname == "/about" ? "underline" : "text-white"}`}
+        >
+          About Us
+        </Link>
+      </MenubarMenu>
+      {pathname == "/sign-in" ||
+      pathname == "/sign-up" ||
+      session.data !== null ? (
+        ""
+      ) : (
+        <>
+          <MenubarMenu>
+            <Dialog open={isLoginDialogOpen} onOpenChange={setLoginDialogOpen}>
+              {pathname == "/sign-up" ? (
+                <Button
+                  className="bg-transparent border-none shadow-none text-black hover:bg-gray-100 hover:shadow-lg"
+                  onClick={() => {
+                    router.replace("/sign-in");
+                  }}
+                >
+                  Login
+                </Button>
+              ) : (
+                <DialogTrigger asChild>
+                  <Button className="bg-transparent border-none shadow-none text-white hover:bg-black hover:shadow-lg">
+                    Login
+                  </Button>
+                </DialogTrigger>
+              )}
+              <DialogContent className="sm:max-w-[425px] 500px:w-[80%]  ">
+                <DialogHeader>
+                  <DialogTitle>Login</DialogTitle>
+                </DialogHeader>
+                <Login onClose={() => setLoginDialogOpen(false)} />
+              </DialogContent>
+            </Dialog>
+          </MenubarMenu>
+          <MenubarMenu>
+            <Dialog
+              open={isSignupDialogOpen}
+              onOpenChange={setSignupDialogOpen}
+            >
+              <DialogTrigger asChild>
+                <Button className="bg-transparent border-none shadow-none text-white hover:bg-black hover:shadow-lg">
+                  Signup
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="my-5 sm:max-w-[425px]">
+                <DialogHeader>
+                  <DialogTitle>SignUp</DialogTitle>
+                  <DialogDescription>Signup to get started.</DialogDescription>
+                </DialogHeader>
+                {/* Pass onClose function */}
+                <Signup onClose={() => setSignupDialogOpen(false)} />{" "}
+              </DialogContent>
+            </Dialog>
+          </MenubarMenu>
+        </>
+      )}
+      {(session.data?.user?._role === "6706bc9fff27bd499083aac2" ||
+        session.data?.user?._role === "6706bd8dff27bd499083aac3") && (
+        <MenubarMenu>
+          <Link
+            href={"/admin/dashboard"}
+            className={`${
+              pathname == "/admin/dashboard" ? "underline" : "text-white"
+            }`}
+          >
+            Admin
+          </Link>
+        </MenubarMenu>
+      )}
+      {session.data && (
+        <>
+          {console.log(session.data.user)}
+          <MenubarMenu>
+            <Button
+              className="bg-transparent border-none shadow-none text-white hover:bg-gray-100 hover:shadow-lg hover:text-black p-2"
+              onClick={() => {
+                signOut({ callbackUrl: "/sign-in" });
+              }}
+            >
+              Logout
+            </Button>
+          </MenubarMenu>
+          <MenubarMenu>| Hi, {session.data.user._username}</MenubarMenu>
+        </>
+      )}
+    </>
+  );
+}
+
+export default NavElement;
