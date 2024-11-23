f68ca98dd3c02c3d320033ac68038be3845a50ae
aayushdhiman <aayush70dhiman@gmail.com>
2024-11-23 16:44:31 +0530

    finised dashboard

--- src/components/ui/Admin/NavBar.jsx
+++ src/components/ui/Admin/NavBar.jsx
@@ -29,7 +29,7 @@ const AdminNavBar = () => {
               alt="logo"
             ></Image>
           </Link>
-          <div className="flex justify-center items-center box-border rounded-2xl hover:bg-slate-500 bg-slate-700 right-3">
+          <div className="flex justify-center items-center right-3">
             <ThemeToggle />
           </div>
         </div>
